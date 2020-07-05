390abf8052ba9b82bd906f497b84f8e086177886
Bruno Lucas <bruno.melo@b2wdigital.com>
2020-07-05 20:41:48 -0300

    init

---
+++ src/main/java/com/templatecloud/transactionauthserver/TransactionAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.templatecloud.transactionauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/templatecloud/transactionauthserver/TransactionAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.templatecloud.transactionauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
